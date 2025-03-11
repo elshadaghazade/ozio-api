43a265788796b4919b1d72d734e9f265ea1d3599
USER <user@USERs-MacBook-Pro.local>
2025-03-11 08:37:06 +0100

    comimt

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,13 @@ module.exports = {
     apps: [
         {
             name: "online-market-backend",
-            script: "dist/server.js",
+            script: "npm",
+            args: "start",
             instances: "3",
             exec_mode: "cluster",
             watch: false,
             max_memory_restart: "500M",
-            log_date_format: "",
+            log_date_format: "YYYY-MM-DD HH:mm:ss",
             error_file: "./logs/error.log",
             out_file: "./logs/output.log",
             combine_logs: true,
